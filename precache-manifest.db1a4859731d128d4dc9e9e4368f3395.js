bdc0a7913fff89c395b98ef44042efa08b21eb12
Shaked Ashkenazi <shaked.a21@gmail.com>
2020-05-16 17:42:55 +0300

    Updates

---
+++ precache-manifest.db1a4859731d128d4dc9e9e4368f3395.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4142a406567a05da0c364f5c2a46a2c",
+    "url": "/rTerminal/index.html"
+  },
+  {
+    "revision": "e015394b93ab25920956",
+    "url": "/rTerminal/static/css/2.fed23d4a.chunk.css"
+  },
+  {
+    "revision": "b0d2df1cd0afe28a406f",
+    "url": "/rTerminal/static/css/main.a45246ea.chunk.css"
+  },
+  {
+    "revision": "e015394b93ab25920956",
+    "url": "/rTerminal/static/js/2.44db16de.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/rTerminal/static/js/2.44db16de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d2df1cd0afe28a406f",
+    "url": "/rTerminal/static/js/main.d2ff7581.chunk.js"
+  },
+  {
+    "revision": "e54f9911883e5b159212",
+    "url": "/rTerminal/static/js/runtime-main.b4962ff7.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/rTerminal/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/rTerminal/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/rTerminal/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/rTerminal/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/rTerminal/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/rTerminal/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/rTerminal/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/rTerminal/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/rTerminal/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/rTerminal/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/rTerminal/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/rTerminal/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/rTerminal/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/rTerminal/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/rTerminal/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/rTerminal/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
